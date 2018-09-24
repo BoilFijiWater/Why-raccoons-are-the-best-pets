d647a157683079531bee271e1f6951fb1cd3e516
SiLordOfLight <jatnerd@gmail.com>
2018-09-24 10:36:39 -0700

    afd

--- script.js
+++ script.js
@@ -4,5 +4,41 @@ $(document).ready(function() {
 	$("#ri").click(function()  {
 	$("#ir").show();	
     });
+	
+	$("#mr").click(function()  {
+	$("#mathr").show();	
+    });
+	
+	$("#br").click(function()  {
+	$("#cr").show();	
+    });
+	
+	$("#cr").click(function()  {
+	$("#compr").show();	
+    });
+	
+	$("#cleanr").click(function()  {
+	$("#broomr").show();	
+    });
+	
+	$("#rw").click(function()  {
+	$("#weedingr").show();	
+    });
+	
+	$("#rtm").click(function()  {
+	$("#tmr").show();	
+    });
+	
+	$("#rd").click(function()  {
+	$("#driver").show();	
+    });
+	
+	$("#rfs").click(function()  {
+	$("#fashionr").show();	
+    });
+	
+	$("#rms").click(function()  {
+	$("#movier").show();	
+    });
 });
 
