30e2a2ed6eb4f33b44558a0c3cff8fc67aa56425
BoiledFijiWaer <42652947+BoilFijiWater@users.noreply.github.com>
2018-09-23 19:42:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,36 @@ $(document).ready(function() {
 	$("#ri").click(function()  {
 	$("#ir").show();	
     });
+	
+	$("#oi").click(function()  {
+	$("#mr").show();	
+    });
+	$("#pi").click(function()  {
+	$("#br").show();	
+    });
+	$("#ai").click(function()  {
+	$("#ir").show();	
+    });
+	$("#si").click(function()  {
+	$("#cr").show();	
+    });
+	$("#di").click(function()  {
+	$("#cleanr").show();	
+    });
+	$("#fi").click(function()  {
+	$("#wr").show();	
+    });
+	$("#gi").click(function()  {
+	$("#tmi").show();	
+    });
+	$("#hi").click(function()  {
+	$("#di").show();	
+    });
+	$("#ji").click(function()  {
+	$("#si").show();	
+    });
+	$("#ji").click(function()  {
+	$("#fr").show();	
+    });
 });
 
